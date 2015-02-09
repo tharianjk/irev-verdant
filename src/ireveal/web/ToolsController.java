11d9d5b49a39cc916068f258345ebb901294ca84
preethasanthosh <preethasanthosh@gmail.com>
2015-02-09 16:01:54 +0530

    Polar freq unit

--- src/ireveal/web/ToolsController.java
+++ src/ireveal/web/ToolsController.java
@@ -116,8 +116,8 @@ public class ToolsController implements Controller {
         		List<TestFrequency> freqlist=this.mastersservice.getFreqList(Integer.parseInt(testid));
         		for (int i=0;i<freqlist.size();i++){
         			if(i==0)
-        				{strfreqs=freqlist.get(i).getFrequencyid()+"";}
-        			else {strfreqs=strfreqs+","+freqlist.get(i).getFrequencyid();}
+        				{strfreqs=freqlist.get(i).getFrequency()+"";}
+        			else {strfreqs=strfreqs+","+freqlist.get(i).getFrequency();}
         		}
         		if(atype.equals("E") && ptype.equals("L") )
         		{
