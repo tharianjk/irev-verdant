b17660a45d3cc94847944453ab242958ec6ab9e2
preethasanthosh <preethasanthosh@gmail.com>
2016-03-09 16:30:44 +0530

    removed menu select for new serial

--- db/pv_procedures.sql
+++ db/pv_procedures.sql
@@ -1025,6 +1025,8 @@ DELIMITER ;
 -- --------------------------------------------------------------------------------
 -- drop procedure if exists spPV_BSBL;
 
+-- drop procedure if exists spPV_BSBL;
+
 DELIMITER $$
 
 CREATE  PROCEDURE spPV_BSBL(
@@ -1039,62 +1041,62 @@ BEGIN
 
 
 # 1. Set procedure id. This is given to identify the procedure in log. Give the procedure name here
-	declare l_proc_id varchar(100) default 'spPV_BSBL';
+    declare l_proc_id varchar(100) default 'spPV_BSBL';
 
 # 2. declare variable to store debug flag
     declare isDebug INT default 0;
 
 
 # 3. declare continue/exit handlers for logging SQL exceptions/errors :
--- write handlers for specific known error codes which are likely to occur here    
+-- write handlers for specific known error codes which are likely to occur here   
 -- eg : DECLARE CONTINUE HANDLER FOR 1062
--- begin 
--- 	if isDebug > 0 then
--- 		call debug(l_proc_id, 'Duplicate keys error encountered','E','I');
--- 	end if;
+-- begin
+--     if isDebug > 0 then
+--         call debug(l_proc_id, 'Duplicate keys error encountered','E','I');
+--     end if;
 -- end;
 
 -- write handlers for sql states which occur due to one or more sql errors here
--- eg : DECLARE EXIT HANDLER FOR SQLSTATE '23000' 
+-- eg : DECLARE EXIT HANDLER FOR SQLSTATE '23000'
  -- begin
--- 	if isDebug > 0 then
--- 		call debug(l_proc_id, 'SQLSTATE 23000','F','I');
--- 	end if;
+--     if isDebug > 0 then
+--         call debug(l_proc_id, 'SQLSTATE 23000','F','I');
+--     end if;
 -- end;
  
  -- write handlers for generic SQL exception which occurs due to one or more SQL states
 
- DECLARE EXIT HANDLER FOR SQLEXCEPTION 
+ DECLARE EXIT HANDLER FOR SQLEXCEPTION
  begin
-	if isDebug > 0 then
-		GET DIAGNOSTICS CONDITION 1 @sqlstate = RETURNED_SQLSTATE, 
-		@errno = MYSQL_ERRNO, @text = MESSAGE_TEXT;
-		SET @full_error = CONCAT("SQLException ", @errno, " (", @sqlstate, "): ", @text);
-		call debug(l_proc_id, @full_error,'F','I');
-        SET @details = CONCAT("Test id : ", testid, ", deg : ",deg, ", typ : ",typ,",serialid : ",serialid);
-		call debug(l_proc_id, @details,'I','I');
-        
+    if isDebug > 0 then
+        GET DIAGNOSTICS CONDITION 1 @sqlstate = RETURNED_SQLSTATE,
+        @errno = MYSQL_ERRNO, @text = MESSAGE_TEXT;
+        SET @full_error = CONCAT("SQLException ", @errno, " (", @sqlstate, "): ", @text);
+        call debug(l_proc_id, @full_error,'F','I');
+        SET @details = CONCAT("Test id : ", testid, ", deg : ",deg, ", typ : ",vdatatype,",serialid : ",serialid);
+        call debug(l_proc_id, @details,'I','I');
+       
          RESIGNAL set MESSAGE_TEXT = 'Exception encountered in the inner procedure';
-	end if;
+    end if;
  end;
 
-# 4. store the debug flag 
+# 4. store the debug flag
 select ndebugFlag into isDebug from fwk_company;
-  
+ 
 if isDebug > 0 then
-	call debug(l_proc_id,'in spPV_BSBL','I','I');
+    call debug(l_proc_id,'in spPV_BSBL','I','I');
  end if;
 
 
 -- select nprecision into prec from fwk_company;
 
  select frequnit into @unt from pv_testdata where test_id=testid;
-  select distinct case @unt when 'GHz' then frequency/1000 else frequency end frequency, 
+  select distinct case @unt when 'GHz' then frequency/1000 else frequency end frequency,
    round( case deg when '0' then 3Db_BW_0_left else 3Db_BW_BMax_left end,prec) ldbpoint,
     round(case deg when '0' then 3Db_BW_0_right else 3Db_BW_BMax_right end,prec) rdbpoint ,
     round(case deg when '0' then 3Db_BS_0 else 3Db_BS_BMax end,prec) BS ,
     round(X1,prec) X1, round(Y1,prec) Y1, round(Backlobe,prec) Backlobe,s.serialno, '' remark
-    from pv_cpcalculated c inner join pv_prodserial s on c.prodserial_id=s.prodserial_id where p.prodserial_id=serialid and datatype=vdatatype ;
+    from pv_cpcalculated c inner join pv_prodserial s on c.prodserial_id=s.prodserial_id where c.prodserial_id=serialid and datatype=vdatatype ;
 
 
 
